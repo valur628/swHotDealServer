8c481a95a5de2f8c72bb2006a5ffc3eceb5a5213
wncjf2000 <valurauta628@gmail.com>
2021-04-21 16:17:23 +0900

    험블번들 완성

--- functions/index.js
+++ functions/index.js
@@ -19,37 +19,34 @@ let HB_Split = [
 	['"full_price":{"currency":"USD","amount":', '}'],
 	['"current_price":{"currency":"USD","amount":', '}'],
 	['DONT', ''],
+	['"human_url":"', '"'],
 	['DONT', ''],
-	['DONT', ''],
-	['&amp;h=206&amp;w=360&amp;', '"'],
-	['&amp;h=206&amp;w=360&amp;', '"']
+	['"featured_image_recommendation":"https://hb.imgix.net/', '"'],
+	['"large_capsule":"https://hb.imgix.net/', '"']
 ]
 let html;
-let fileOutput = "";
+let fileOutput;
+let lineTotal;
+
 app.use(morgan('combined'));
 app.listen(8080, () => {
 	console.log('hosted on 8080');
 });
-async function humblebundleWeb() {
+async function humblebundleWeb(pageNum) {
 	let splitValue = ["abcdefgh"];
-	const browser = await puppeteer.launch({
-		headless: true
-	});
+	const browser = await puppeteer.launch({ headless: true });
 	const page = await browser.newPage();
 	await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.128 Safari/537.36');
-	await page.goto('https://www.humblebundle.com/store/api/search?sort=bestselling&filter=all&genre=software&page=0&request=1', {
-		waitUntil: 'networkidle0'
-	});
+	await page.goto('https://www.humblebundle.com/store/api/search?sort=bestselling&filter=all&genre=software&page=' + pageNum + '&request=1', { waitUntil: 'networkidle0' });
 	html = await page.content();
+	await page.waitFor(500);
 	splitValue = html.split('standard_carousel_image":"');
-	console.log("처음 " + splitValue[0]);
 	page.close();
 	browser.close();
-	console.log("다음 " + splitValue[0]);
 	return splitValue;
 }
 
-function humblebundleDB(splitValue, i) {
+function humblebundleDB(splitValue, lines, pageNum) {
 	let FB_object = {
 		DB_LoadNumber: 0,
 		DB_SWName: "Not SW Name",
@@ -64,41 +61,61 @@ function humblebundleDB(splitValue, i) {
 		DB_RepPicture: "Not Main Picture",
 		DB_OthPicture: "Not Sub Picture"
 	}
-	FB_object.DB_LoadNumber = i;
-	FB_object.DB_SWName = (splitValue[i].split(HB_Split[0][0])[1].toString()).split(HB_Split[0][1])[0].toString();
+	FB_object.DB_LoadNumber = lineTotal;
+
+	nameTemp = (splitValue[lines].split(HB_Split[0][0])[1].toString()).split(HB_Split[0][1])[0].toString();
+	nameTemp = nameTemp.replace(/&amp;/g, "&");
+	nameTemp = nameTemp.replace(/&2122/g, "");
+	nameTemp = nameTemp.replace(/^[\s\u00a0\u3000]+|[\s\u00a0\u3000]+$/g, "")
+	nameTemp = nameTemp.replace(/\\u00a0/g, " ");
+	FB_object.DB_SWName = nameTemp.replace(/\\u/g, "");
+
 	FB_object.DB_DevName = "Not Dev";
 	FB_object.DB_DisPeriod = 20000101;
-	FB_object.DB_Currecny = (splitValue[i].split(HB_Split[3][0])[1].toString()).split(HB_Split[3][1])[0].toString();
-	FB_object.DB_Cost = (splitValue[i].split(HB_Split[4][0])[1].toString()).split(HB_Split[4][1])[0].toString();
-	FB_object.DB_DisPrice = (splitValue[i].split(HB_Split[5][0])[1].toString()).split(HB_Split[5][1])[0].toString();
-	FB_object.DB_DisRate = (Math.round(((parseInt(FB_object.DB_DisPrice) / parseInt(FB_object.DB_Cost)) * 100) * 10) / 10) - 100;
-	FB_object.DB_PlatAddress = "https://www.humblebundle.com/";
+	FB_object.DB_Currecny = (splitValue[lines].split(HB_Split[3][0])[1].toString()).split(HB_Split[3][1])[0].toString();
+	FB_object.DB_Cost = (splitValue[lines].split(HB_Split[4][0])[1].toString()).split(HB_Split[4][1])[0].toString();
+	FB_object.DB_DisPrice = (splitValue[lines].split(HB_Split[5][0])[1].toString()).split(HB_Split[5][1])[0].toString();
+	FB_object.DB_DisRate =  100 - (Math.round(((parseInt(FB_object.DB_DisPrice) / parseInt(FB_object.DB_Cost)) * 100) * 10) / 10);
+	FB_object.DB_PlatAddress = "https://www.humblebundle.com/store/" + (splitValue[lines].split(HB_Split[7][0])[1].toString()).split(HB_Split[7][1])[0].toString();
 	FB_object.DB_PlatName = "HumbleBundle";
-	FB_object.DB_RepPicture = "https://hb.imgix.net/880607b972d9cb3b02ebea9299905bd94c532edd.jpeg?auto=compress,format&fit=crop&h=206&w=360&" + splitValue[i].substring(splitValue[i].indexOf(HB_Split[9][0]) + 1, splitValue[i].indexOf(HB_Split[9][1]));
-	FB_object.DB_OthPicture = "https://hb.imgix.net/880607b972d9cb3b02ebea9299905bd94c532edd.jpeg?auto=compress,format&fit=crop&h=206&w=360&" + splitValue[i].substring(splitValue[i].indexOf(HB_Split[10][0]) + 1, splitValue[i].indexOf(HB_Split[10][1]));
-	fileOutput += JSON.stringify(FB_object, null, 5) + (i == splitValue.length - 1 ? "]}" : ",");
+
+	pictureTemp = 'https://hb.imgix.net/' + (splitValue[lines].split(HB_Split[9][0])[1].toString()).split(HB_Split[9][1])[0].toString();
+	FB_object.DB_RepPicture = pictureTemp.indexOf("&amp;") == -1 ? pictureTemp : pictureTemp.replace(/&amp;/g, "&");
+
+	pictureTemp = 'https://hb.imgix.net/' + (splitValue[lines].split(HB_Split[10][0])[1].toString()).split(HB_Split[10][1])[0].toString();
+	FB_object.DB_OthPicture = pictureTemp.indexOf("&amp;") == -1 ? pictureTemp : pictureTemp.replace(/&amp;/g, "&");
+
+	return JSON.stringify(FB_object, null, 5) + (lineTotal == (pageNum * 20) - 1 ? "]}" : ",");
 	/*fs.appendFile('result.json', JSON.stringify(FB_object, null, 5) + (i == splitValue.length-1 ? "]}" : ","), 'utf8', function(error) {
 	    console.log(error);
 	});*/
 }
 async function humblebundleMain() {
-	splitValue = [];
+	let splitValue = [];
+	fileOutput = '';
+	pageNum = 5;
+	fs.writeFile('result.json', '{"HB_ObjName":"HumbleBundle","HB_Software": [', 'utf8', function(error) {
+		console.log(error);
+	});
 	/*fs.writeFile('result.json', "", 'utf8', function(error) {
 	    console.log('write start');
 	});*/
 	/*fs.appendFile('result.json', '{"HB_ObjName":"HumbleBundle","HB_Software": [', 'utf8', function(error) {
 	    console.log(error);
 	});*/
-	fileOutput += '{"HB_ObjName":"HumbleBundle","HB_Software": [';
-	splitValue = await humblebundleWeb();
+	
+	for(let i = 0; i < pageNum; i++) {
+	splitValue = await humblebundleWeb(i);
 	console.log("다다음 " + splitValue);
-	for(let i = 1; i < splitValue.length - 1; i++) {
-		await humblebundleDB(splitValue, i);
+	for(let j = 1; j < splitValue.length; j++) {
+		lineTotal = (j + (i * 20) - 1);
+		fileOutput += await humblebundleDB(splitValue, j, pageNum);
 	}
-	await humblebundleDB(splitValue, splitValue.length - 1);
-	fs.writeFile('result.json', fileOutput, 'utf8', function(error) {
-		console.log(error);
-	});
+	}
+	//await humblebundleDB(splitValue, (pageNum * 20) - 1, pageNum);
+		fs.appendFile('result.json', fileOutput, 'utf8', function(error) {
+			console.log(error);
+		});
 	/*fs.appendFile('result.json', "]}", 'utf8', function(error) {
 	    console.log("end");
 	});*/
